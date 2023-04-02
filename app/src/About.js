3da87167930da7d9d87142a6a2a1892cf1a82292
reeman9000 <preston.soper@student.awesomeinc.org>
2023-04-02 12:13:11 +0000

    added card background to about section

--- app/src/About.js
+++ app/src/About.js
@@ -8,8 +8,10 @@ function About() {
         <Logo />
         <h2 className="text-center">About Us</h2>
         <div className="container">
+            <div className="about card">
         <h3 className="text-center">Bookapedia is a website that allows you to search through the vast amount of books from the Google Books API. Traverse a fresh, modern UI,
         find books to purchase and save books to your wishlist for later!</h3>
+        </div>
         </div>
         </>
     )
